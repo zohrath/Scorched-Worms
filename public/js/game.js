1394c358dd29eecec9d2e1dc47af2318579c2699
CasparLindvall <caspar08@gmail.com>
2018-11-12 15:29:54 +0100

    spawn n move 2 tanks

--- public/js/game.js
+++ public/js/game.js
@@ -22,6 +22,8 @@ var game = new Phaser.Game(config);
 
 function preload() {
   this.load.image('tank', 'assets/tank-large.png');
+  this.load.image('otherPlayer', 'assets/tank-large.png');
+
 }
 
 function create() {
@@ -52,14 +54,80 @@ function create() {
     });
   });
 
+  this.socket.on('playerMoved', function (playerInfo) {
+    self.otherPlayers.getChildren().forEach(function (otherPlayer) {
+      if (playerInfo.playerId === otherPlayer.playerId) {
+        otherPlayer.setRotation(playerInfo.rotation);
+        otherPlayer.setPosition(playerInfo.x, playerInfo.y);
+      }
+    });
+  });
+
   this.cursors = this.input.keyboard.createCursorKeys();
 }
 
 function addPlayer(self, playerInfo) {
 
   self.tank = self.physics.add.image(playerInfo.x, playerInfo.y, 'tank').setOrigin(0.5, 0.5).setDisplaySize(53, 40);
+/*
+  self.ship.setDrag(100);
+  self.ship.setAngularDrag(100);
+  self.ship.setMaxVelocity(200);
 
+*/
 
 }
 
-function update() {}
+function addOtherPlayers(self, playerInfo) {
+  const otherPlayer = self.add.sprite(playerInfo.x, playerInfo.y, 'otherPlayer').setOrigin(0.5, 0.5).setDisplaySize(53, 40);
+  if (playerInfo.team === 'blue') {
+    otherPlayer.setTint(0x0000ff);
+  } else {
+    otherPlayer.setTint(0xff0000);
+  }
+  otherPlayer.playerId = playerInfo.playerId;
+  self.otherPlayers.add(otherPlayer);
+}
+
+function update() {
+
+//    velocityFromAngle: function (angle, speed, vec2)
+//      this.player.body.velocity.x = 200;
+
+
+if (this.tank) {
+    if (this.cursors.left.isDown) {
+        this.tank.body.velocity.x = -150;
+    } else if (this.cursors.right.isDown) {
+        this.tank.body.velocity.x = 150;
+    } else {
+        this.tank.body.velocity.x = 0;
+    }
+  
+    if (this.cursors.up.isDown) {
+      //Set angle
+    } else {
+      this.tank.setAcceleration(0);
+    }
+
+    // emit player movement
+    var x = this.tank.x;
+    var y = this.tank.y;
+    var r = this.tank.rotation;
+    if (this.tank.oldPosition && (x !== this.tank.oldPosition.x || y !== this.tank.oldPosition.y || r !== this.tank.oldPosition.rotation)) {
+      this.socket.emit('playerMovement', { x: this.tank.x, y: this.tank.y, rotation: this.tank.rotation });
+    }
+     
+    // save old position data
+    this.tank.oldPosition = {
+      x: this.tank.x,
+      y: this.tank.y,
+      rotation: this.tank.rotation
+    };
+  
+    this.physics.world.wrap(this.tank, 5);
+  }
+
+
+
+}
--- server.js
+++ server.js
@@ -25,6 +25,8 @@ io.on('connection', function (socket) {
   };
   // send the players object to the new player
   socket.emit('currentPlayers', players);
+   // update all other players of the new player
+  socket.broadcast.emit('newPlayer', players[socket.id]);
 
   // when a player disconnects, remove them from our players object
   socket.on('disconnect', function () {
@@ -34,6 +36,15 @@ io.on('connection', function (socket) {
     // emit a message to all players to remove this player
     io.emit('disconnect', socket.id);
 
+  });
+
+   // when a player moves, update the player data
+  socket.on('playerMovement', function (movementData) {
+    players[socket.id].x = movementData.x;
+    players[socket.id].y = movementData.y;
+    players[socket.id].rotation = movementData.rotation;
+    // emit a message to all players about the player that moved
+    socket.broadcast.emit('playerMoved', players[socket.id]);
   });
 
 });
