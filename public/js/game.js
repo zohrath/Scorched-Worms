f788fde0559e570f117826f95d1bb72a03bad4f4
nurre <nurre13@gmail.com>
2018-11-21 16:03:21 +0100

    Added a basic menu. The game is now a scene.

--- public/js/game.js
+++ public/js/game.js
@@ -1,24 +1,3 @@
-let config = {
-  type: Phaser.AUTO,
-  parent: "ScorchedWorms",
-  width: 1024,
-  height: 768,
-  physics: {
-    default: "arcade",
-    arcade: {
-      debug: true,
-      gravity: { y: 300 }
-    }
-  },
-  scene: {
-    preload: preload,
-    create: create,
-    update: update
-    //render:?
-  }
-};
-
-let game = new Phaser.Game(config);
 let platforms;
 let cursors;
 let keyX;
@@ -30,53 +9,57 @@ let power = 0;
 let mouseAngle = 0;
 let socket;
 
-function preload() {
-  this.load.image("tank_right", "assets/tank_right.png");
-  this.load.image("tank_left", "assets/tank_left.png");
-  this.load.image("tank", "assets/tank_right.png");
-  this.load.image("background", "assets/background_vulcano.png");
-  this.load.image("ground", "assets/ground.png");
-  this.load.image("turret", "assets/turret.png");
-  this.load.image("smoke", "assets/smoke-puff.png");
-  this.load.image("bullet", "assets/bullet.png");
-}
+class GameScene extends Phaser.Scene{
+  constructor ()
+    {
+        super({ key: 'GameScene' });
+    }
 
-function create() {
-  this.nextTic = 0;
-  let self = this;
-  this.background = this.add.sprite(512, 384, "background");
-  this.bullets = this.physics.add.group({
-    classType: Bullet,
-    runChildUpdate: true
-  });
-  platforms = this.physics.add.staticGroup();
-  platforms.create(512, 753, "ground");
+  preload () {
+    this.load.image("tank_right", "assets/tank_right.png");
+    this.load.image("tank_left", "assets/tank_left.png");
+    this.load.image("tank", "assets/tank_right.png");
+    this.load.image("background", "assets/background_vulcano.png");
+    this.load.image("ground", "assets/ground.png");
+    this.load.image("turret", "assets/turret.png");
+    this.load.image("smoke", "assets/smoke-puff.png");
+    this.load.image("bullet", "assets/bullet.png");
+  }
 
-  powerText = this.add.text(16, 16, "Power: 0", {
-    fontSize: "32px",
-    fill: "#999"
-  });
+  create () {
+    this.nextTic = 0;
+    let self = this;
+    this.background = this.add.sprite(512, 384, "background");
+    this.bullets = this.physics.add.group({
+      classType: Bullet,
+      runChildUpdate: true
+    });
+    platforms = this.physics.add.staticGroup();
+    platforms.create(512, 753, "ground");
 
-  this.physics.world.setBoundsCollision(true, true, false, true);
-  keyX = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.X);
-  cursors = this.input.keyboard.createCursorKeys();
+    powerText = this.add.text(16, 16, "Power: 0", {
+      fontSize: "32px Arial",
+      fill: "#999"
+    });
 
-  socket = io();
-  this.otherPlayers = this.physics.add.group();
+    this.physics.world.setBoundsCollision(true, true, false, true);
+    keyX = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.X);
+    cursors = this.input.keyboard.createCursorKeys();
 
-
-  createSocketListners(self);
-  //COLLIDERS
-  this.physics.add.collider(this.bullets, platforms, explodeBullet, null, self);
-  this.physics.add.collider(
-    this.bullets,
-    this.otherPlayers,
-    explodeBullet,
-    null,
-    self
-  );
+    socket = io();
+    this.otherPlayers = this.physics.add.group();
 
 
+    createSocketListners(self);
+    //COLLIDERS
+    this.physics.add.collider(this.bullets, platforms, explodeBullet, null, self);
+    this.physics.add.collider(
+      this.bullets,
+      this.otherPlayers,
+      explodeBullet,
+      null,
+      self
+    );
 
   this.input.on(
     "pointermove",
@@ -93,7 +76,94 @@ function create() {
     },
     this
   );
+  }
+
+  update(time, delta) {
+    if (typeof playerContainer !== "undefined" && playerContainer.active) {
+      turretInContainer.rotation = mouseAngle;
+      if (cursors.left.isDown) {
+        playerContainer.body.setAccelerationX(-500);
+      } else if (cursors.right.isDown) {
+        playerContainer.body.setAccelerationX(500);
+      } else {
+        playerContainer.body.setAccelerationX(0);
+      }
+      if (cursors.up.isDown && playerContainer.body.touching.down) {
+        playerContainer.body.velocity.y = -100;
+      } else if (cursors.down.isDown) {
+        turretInContainer.rotation--;
+      }
+
+      // The if statement below this is never true. Something is wrong with keyX.
+      if (keyX.isdown && playerContainer.body.touching.down) {
+        playerContainer.body.velocity.y = -100;
+      }
+  
+      // SPACE
+      if (cursors.space.isDown) {
+        this.spaceDown = true;
+        power = (power + Math.floor(delta / 2)) % 1000;
+        powerText.setText("Power: " + power);
+      } else if (cursors.space.isUp) {
+        if (this.spaceDown && time > this.nextTic) {
+          this.nextTic = time + 500;
+          let shotInfo = {
+            power: power,
+            angle: turretInContainer.rotation
+          };
+          socket.emit("bulletFired", shotInfo);
+  
+          this.spaceDown = false;
+          power = 0;
+        }
+      }
+  
+      // emit player movement
+      if (playerContainer.oldPosition) {
+        if (
+          playerContainer.x !== playerContainer.oldPosition.x ||
+          playerContainer.y !== playerContainer.oldPosition.y ||
+          playerContainer.rotation !== playerContainer.oldPosition.rotation
+        ) {
+          socket.emit("playerMovement", {
+            x: playerContainer.x,
+            y: playerContainer.y,
+            rotation: playerContainer.rotation
+          });
+        }
+  
+        if (
+          turretInContainer.rotation !== playerContainer.oldPosition.turretRotation
+        ) {
+          socket.emit("toOtherClients", {
+            event: "moveTurret",
+            turretRotation: playerContainer.oldPosition.turretRotation
+          });
+        }
+      }
+      // save old position data
+      playerContainer.oldPosition = {
+        x: playerContainer.x,
+        y: playerContainer.y,
+        rotation: playerContainer.rotation,
+        turretRotation: turretInContainer.rotation
+      };
+      if (playerContainer.body.velocity.x > 0) {
+        this.emitter.startFollow(playerContainer, -30, 8);
+        playerContainer.list[0].flipX = false;
+        this.emitter.on = true;
+      } else if (playerContainer.body.velocity.x < 0) {
+        this.emitter.startFollow(playerContainer, 30, 8);
+        playerContainer.list[0].flipX = true;
+        this.emitter.on = true;
+      } else {
+        this.emitter.on = false;
+      }
+    }
+  }
+
 }
+
 
 function createTank(self, playerInfo) {
   console.log("Adding player!")
@@ -147,89 +217,6 @@ function addOtherPlayer(self, playerInfo) {
   self.otherPlayers.add(otherPlayer);
 }
 
-function update(time, delta) {
-  if (typeof playerContainer !== "undefined" && playerContainer.active) {
-    turretInContainer.rotation = mouseAngle;
-    if (cursors.left.isDown) {
-      playerContainer.body.setAccelerationX(-500);
-    } else if (cursors.right.isDown) {
-      playerContainer.body.setAccelerationX(500);
-    } else {
-      playerContainer.body.setAccelerationX(0);
-    }
-    if (cursors.up.isDown && playerContainer.body.touching.down) {
-      playerContainer.body.velocity.y = -100;
-    } else if (cursors.down.isDown) {
-      turretInContainer.rotation--;
-    }
-    // The if statement below this is never true. Something is wrong with keyX.
-    if (keyX.isdown && playerContainer.body.touching.down) {
-      playerContainer.body.velocity.y = -100;
-    }
-
-    // SPACE
-    if (cursors.space.isDown) {
-      this.spaceDown = true;
-      power = (power + Math.floor(delta / 2)) % 1000;
-      powerText.setText("Power: " + power);
-    } else if (cursors.space.isUp) {
-      if (this.spaceDown && time > this.nextTic) {
-        this.nextTic = time + 500;
-        shotInfo = {
-          power: power,
-          angle: turretInContainer.rotation
-        };
-        socket.emit("bulletFired", shotInfo);
-
-        this.spaceDown = false;
-        power = 0;
-      }
-    }
-
-    // emit player movement
-    if (playerContainer.oldPosition) {
-      if (
-        playerContainer.x !== playerContainer.oldPosition.x ||
-        playerContainer.y !== playerContainer.oldPosition.y ||
-        playerContainer.rotation !== playerContainer.oldPosition.rotation
-      ) {
-        socket.emit("playerMovement", {
-          x: playerContainer.x,
-          y: playerContainer.y,
-          rotation: playerContainer.rotation
-        });
-      }
-
-      if (
-        turretInContainer.rotation !== playerContainer.oldPosition.turretRotation
-      ) {
-        socket.emit("toOtherClients", {
-          event: "moveTurret",
-          turretRotation: playerContainer.oldPosition.turretRotation
-        });
-      }
-    }
-    // save old position data
-    playerContainer.oldPosition = {
-      x: playerContainer.x,
-      y: playerContainer.y,
-      rotation: playerContainer.rotation,
-      turretRotation: turretInContainer.rotation
-    };
-    if (playerContainer.body.velocity.x > 0) {
-      this.emitter.startFollow(playerContainer, -30, 8);
-      playerContainer.list[0].flipX = false;
-      this.emitter.on = true;
-    } else if (playerContainer.body.velocity.x < 0) {
-      this.emitter.startFollow(playerContainer, 30, 8);
-      playerContainer.list[0].flipX = true;
-      this.emitter.on = true;
-    } else {
-      this.emitter.on = false;
-    }
-  }
-}
-
 function explodeBullet(bullet, object) {
   if (object.hasOwnProperty("playerId")) {
     bullet.hide();
@@ -241,3 +228,22 @@ function explodeBullet(bullet, object) {
 function varExists(obj) {
   return obj.hasOwnProper !== "undefined";
 }
+
+
+
+let config = {
+  type: Phaser.AUTO,
+  parent: "ScorchedWorms",
+  width: 1024,
+  height: 768,
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: true,
+      gravity: { y: 300 }
+    }
+  },
+  scene: [MainMenu, GameScene] 
+};
+
+let game = new Phaser.Game(config);
--- public/js/menu.js
+++ public/js/menu.js
@@ -1,53 +1,62 @@
-Game.Mainmenu = function(game) {
-
-};
+console.log("In main.js!");
 
 var menuscreen;
 
-Game.Mainmenu.prototype = {
-    preload: function (game) {
+class MainMenu extends Phaser.Scene{
+    constructor ()
+    {
+        super({ key: 'MainMenu' });
+    }    
+
+    preload () {
         this.load.image("button", "assets/buttonstolen.png");
         this.load.image("menubackground", "assets/menustolen.jpg");
-    },
-
-    create: function (game) {
-        this.createButton(game, game.world.centerX, game.world.centerY + 32, 200, 100, "Start Game", 
-        function(){
-            this.state.start("game");
-        })
-
-        this.createButton(game,game.world.centerX, game.world.centerY + 192, 200, 100, "Exit", 
-        function(){
-            Console.log("Exit!");
-        })
-
-        menuscreen = this.game.add.sprite(game.world.centerX, game.world.centerY - 192, 'menubackground');
-        menuscreen.anchor.setTo(0.5, 0.5);
-
-    },
-    update: function (game) {
-
-    },
-    createButton: function (game, x, y, w, h, string, callback) {
-        var menuButton = this.add.button(x, y, 'button', callback, this, 2, 1, 0);
-        menuButton.anchor.setTo(0.5, 0.5);
-        menuButton.width = w;
-        menuButton.height = h;
-
-
-        var menuButtonText = this.add.text(menuButton.x, menuButton.y, string, {
-            font:"14px Arial",
-            fill: "#fff",
-            align: "center"
-        });
-        menuButtonText.anchor.setTo(0.5, 0.5);
-        
-
+        console.log("Preload in mainmenu");
     }
 
-};
+    create () {
+        this.createButton(game, 500, 500 + 32, 200, 100, "Start Game", 
+        function(){
+            this.scene.start("GameScene");
+        })
+
+        this.createButton(game, 500, 500 + 192, 200, 100, "Exit", 
+        function(){
+            console.log("Exit!");
+        })
+
+        menuscreen = this.add.sprite(512, 384, 'menubackground').setDepth(0);
+        menuscreen.setScale(4);
+
+        menuscreen.setOrigin(0.5, 0.5);
+    }
+
+    createButton (game, x, y, w, h, string, callback) {
+        var menuButton = this.add.sprite(x, y, 'button').setInteractive().setDepth(1);
+        menuButton.setOrigin(0.5, 0.5);
+        menuButton.setScale(0.5);
+        /* menuButton.width = w;
+        menuButton.height = h;
+ */
+        var menuButtonText = this.add.text(menuButton.x, menuButton.y, string, {
+            font:"32px Arial",
+            fill: "#fff",
+            align: "center"
+        }).setDepth(2);
+        menuButtonText.setOrigin(0.5, 0.5);
+        menuButton.on('pointerdown', callback, this);
+    }
+}
 
 
+/* function startGame (self) {
+    console.log("Starting game scene!");
+    self.scene.start('GameScene');
+}
+
+function exitGame () {
+    console.log("You can't exit this game :((");
+} */
 
 
 
