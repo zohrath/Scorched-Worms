134fd2e231c83f2406a85882f94e6ea4564d7012
nurre <nurre13@gmail.com>
2018-11-26 13:03:07 +0100

    Trying out deployment on Heroku

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ expressApp.get("/", (req, res) => {
 
 gameServer.startGameServer(server);
 
-server.listen(8081, () => {
+server.listen(80, () => {
   console.log(`Listening on ${server.address().port}`);
 });
