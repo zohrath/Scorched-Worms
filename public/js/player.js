5d481c3d1aaf8c1364933612ae8e66350f33b9bb
CasparLindvall <caspar08@gmail.com>
2018-11-29 15:41:41 +0100

    Format structure

--- public/js/player.js
+++ public/js/player.js
@@ -1,32 +1,31 @@
 class Player extends Phaser.GameObjects.Container{
     constructor(scene, character, weapon, playerInfo, color) {
         let characterSprite = scene.add.sprite(0, 0, character);
-        let weaponSprite = new Weapon(scene, "turret", "bullet", 1, 1); //scene.add.sprite(0, -7, weapon);
-        console.log(weaponSprite);
-        weaponSprite.setOrigin(0, 0.5);
+        let weaponSprite = new Weapon(scene, "turret", "bullet", 1, 1);
         let playerText = scene.add.text(0, 0, playerInfo.alias, { fontSize: "18px Arial", fill: color, align: "center" });
+       
+        weaponSprite.setOrigin(0, 0.5);
         playerText.setOrigin(0.5, 2);
         super(scene, playerInfo.x, playerInfo.y, []);
-        scene.physics.world.enable(this);
-        scene.physics.add.collider(this, scene.terrain);
-
         
         this.playerId = playerInfo.playerId;
-        this.setSize(64,40);
+        this.setSize(64, 40);
         this.add(characterSprite);
         this.add(weaponSprite);
         this.add(playerText);
+
+        
         scene.physics.world.enable(this);
         this.body.setBounce(0.3).setCollideWorldBounds(true);
         this.body.setMaxVelocity(300).setDragX(300);
+
         scene.physics.add.collider(this, scene.terrain);
-        this.alias = playerInfo.alias
+        this.alias = playerInfo.alias;
         scene.add.existing(this);
     }
 
     fire (scene, angle, power) {
-
-      this.list[1].fire(scene, this.x, this.y, angle, power);  
+      this.list[1].fire(scene, this.x, this.y, angle, power);
     }
 
     getWeaponAngle () {
