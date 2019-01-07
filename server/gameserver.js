fea1d8848600cd016c3e420ef17a6a2f1cd34f84
Adam Woods <adamwoods@student-234-63.eduroam.uu.se>
2019-01-07 14:48:42 +0100

    Test for nextPlayerAlias

--- server/gameserver.js
+++ server/gameserver.js
@@ -161,26 +161,43 @@ function startRoundIfAllReady(playerOrder) {
   if (clientsReady === playerOrder.length && clientsReady > 1) {
     // send the players object to the new player
     console.log("Starting round", playerOrder.alias);
+    currentMap = terrain.createPlatformLayer(WIDTH,HEIGHT,TILESIZE);
     startRound(playerOrder);
     return true;
   }
   return false;
 }
 
-function nextPlayerAlias(playerOrder) {
-  let playerSocketID;
-  do {
-    playerTurnIndex = getNextPlayerTurnIndex(1, playerOrder);
-    playerSocketID = playerOrder[playerTurnIndex];
-  } while (playerSocketID === "DEAD");
-  //console.log(playerSocketID)
-  // TODO check that players ONLY have valid players
-  console.log("playerOrder alias: ", playerOrder);
-  if (players[playerSocketID].alias !== "undefined") {
-    return players[playerSocketID].alias;
-  } else {
-    return;
+function getNextPlayerSocketId(playerOrder, startingIndex) {
+  for (let i = 0; i < playerOrder.length; i++) {
+    const val = (i + startingIndex) % (playerOrder.length - 1);
+    const currentPlayer = playerOrder[val];
+    
+    if (currentPlayer !== "DEAD") {
+      return [currentPlayer, val];
+    }
   }
+}
+
+function getPlayerAlias(socketId, playersTest = players) {
+  try {
+    return playersTest[socketId].alias;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function nextPlayerAlias(playerOrder, startingIndex, playersTest = players) {
+   
+  var alias = playersTest[playerOrder[startingIndex]].alias;
+  
+  if (playerOrder.length > 1) {
+    const id = getNextPlayerSocketId(playerOrder, startingIndex);
+    alias = getPlayerAlias(id[0], playersTest);
+    playerTurnIndex = id[1];
+  
+  }
+  return alias;
 }
 
 function removeFromPlayerOrder(targetID, playerOrder) {
@@ -189,10 +206,6 @@ function removeFromPlayerOrder(targetID, playerOrder) {
       playerOrder[i] = "DEAD"; //remove from index i and 1 element
     }
   });
-}
-
-function getNextPlayerTurnIndex(offset = 1, playerOrder) {
-  return (playerTurnIndex + offset) % playerOrder.length;
 }
 
 function createPlayer(playersObject, id, alias, playerOrder) {
@@ -253,8 +266,12 @@ function newRound(playerOrder) {
 function newTurn(playerOrder, timeout=2000) {
   syncGamestateEmit(io,players,currentMap);
   console.log("playerOrder: ", playerOrder);
+  let x = playerOrder;
+  console.log("x: ", x);
+  let next = nextPlayerAlias(x, playerTurnIndex);
+  console.log("next: ", next);
   setTimeout(function() {
-    io.emit("nextPlayerTurn", nextPlayerAlias(playerOrder));
+    io.emit("nextPlayerTurn", next);
   }, timeout); //delay to sync allowedToEmit and bullet destroy
 }
 
@@ -305,7 +322,9 @@ function resetScene() {
 module.exports = {
   startGameServer,
   getAlivePlayers,
-  startRoundIfAllReady,
   calculateDmg,
-  createPlayer
+  startRoundIfAllReady,
+  createPlayer,
+  getNextPlayerSocketId,
+  nextPlayerAlias
 };
